f927ae6c011212294c830adf5aeb84a4edb4baaf
raigumieri <rai.gumiere@hotmail.com>
2023-09-12 09:46:00 -0300

    Projeto Completo

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.scss";
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <section className="estrutura">
+
+          <article className="box">
+
+            <h1> Nossa estrutura </h1>
+            <img src="http://lorempixel.com.br/350/200/?1" alt="IMG" />
+            <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+
+            <a href="#"> VEJA MAIS </a>
+
+          </article>
+
+          <article className="box">
+
+            <h1> Nossa estrutura </h1>
+            <img src="http://lorempixel.com.br/350/200/?2" alt="IMG" />
+            <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+
+            <a href="#"> VEJA MAIS </a>
+
+          </article>
+
+        </section>     
+      </div>
+
+    </>
+  )
+}
+
+export default App
